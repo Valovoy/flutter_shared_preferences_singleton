44157f23b5945b8f6591a6139e028e6f91dbdf36
Valovoy <aleksandr.valovoy@binerals.com>
2024-01-15 12:27:43 +0200

    Created singleton for shared_preferences

---
+++ android/app/src/main/kotlin/com/devpool/shared_preferences_singleton/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devpool.shared_preferences_singleton
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/shared_preferences_service.dart
@@ -0,0 +1,40 @@
+import 'dart:async' show Future;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  static SharedPreferencesService? _instance;
+  static late SharedPreferences _preferences;
+
+  SharedPreferencesService._();
+
+  // Using a singleton pattern
+  static Future<SharedPreferencesService> getInstance() async {
+    _instance ??= SharedPreferencesService._();
+
+    _preferences = await SharedPreferences.getInstance();
+
+    return _instance!;
+  }
+
+  static dynamic getData(String key) {
+    return _preferences.get(key) ?? 0;
+  }
+
+  static void saveData(String key, dynamic value) {
+    if (value is String) {
+      _preferences.setString(key, value);
+    } else if (value is int) {
+      _preferences.setInt(key, value);
+    } else if (value is double) {
+      _preferences.setDouble(key, value);
+    } else if (value is bool) {
+      _preferences.setBool(key, value);
+    } else if (value is List<String>) {
+      _preferences.setStringList(key, value);
+    }
+  }
+
+  static removeData(String key) {
+    _preferences.remove(key);
+  }
+}
